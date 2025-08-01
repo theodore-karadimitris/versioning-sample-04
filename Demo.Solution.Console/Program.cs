faef49c76dfbe2b3b3c40a83a2099dd59941b2dd
tkar <theodore.karadimitris@iteam.gr>
2025-08-01 13:18:51 +0300

    feat: pr-template (#4)
    * feat!: feature 02 (#2)
    * feat: feature 3 (#3)
    
    BREAKING CHANGE: Dummy
    
    * ci: added PR template

--- Demo.Solution.Console/Program.cs
+++ Demo.Solution.Console/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Feature 01"); 
+Console.WriteLine("Feature 02");
+Console.WriteLine("Feature 03 Breaking");
